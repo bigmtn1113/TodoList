3b51e5e7ab447cb043db7cfc681a3dc0c8531cce
kva231 <kva231@naver.com>
2021-06-30 11:24:03 +0900

    Add login router

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const path = require('path');
 
 dotenv.config();
 const indexRouter = require('./routes');
+const loginRouter = require('./routes/login');
 
 const app = express();
 app.set('port', process.env.PORT);
@@ -13,6 +14,7 @@ app.use(morgan('dev'));
 
 app.use('/static', express.static(__dirname + '/public'));
 app.use('/', indexRouter);
+app.use('/login', loginRouter);
 
 app.listen(app.get('port'), () => {
     console.log(app.get('port'), '번 포트에서 대기 중');
