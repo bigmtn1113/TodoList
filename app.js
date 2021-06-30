3b4cb7c4b3ec58e59126db5916b66dfe7521e4f9
kva231 <kva231@naver.com>
2021-06-30 10:29:34 +0900

    Add index router

--- app.js
+++ app.js
@@ -4,15 +4,14 @@ const dotenv = require('dotenv');
 const path = require('path');
 
 dotenv.config();
+const indexRouter = require('./routes');
 
 const app = express();
 app.set('port', process.env.PORT);
 
 app.use(morgan('dev'));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/html/index.html'));
-});
+app.use('/', indexRouter);
 
 app.listen(app.get('port'), () => {
     console.log(app.get('port'), '번 포트에서 대기 중');
